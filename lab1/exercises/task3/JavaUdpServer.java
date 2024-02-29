f8874426bd1b2f80c737438ff4fa8b03aa6f6f68
Paweł Konop <101901301+marcepanowyy@users.noreply.github.com>
2024-02-29 14:32:45 +0100

    init lab1

---
+++ lab1/exercises/task3/JavaUdpServer.java
@@ -0,0 +1,46 @@
+package org.example.ex3;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+public class JavaUdpServer {
+
+    public static void main(String args[])
+    {
+        System.out.println("JAVA UDP SERVER");
+        DatagramSocket socket = null;
+        int portNumber = 9008;
+
+        try{
+            socket = new DatagramSocket(portNumber);
+            byte[] receiveBuffer = new byte[1024];
+
+            while(true) {
+                Arrays.fill(receiveBuffer, (byte)0);
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                socket.receive(receivePacket);
+
+                int msgNumber = ByteBuffer.wrap(receivePacket.getData()).order(ByteOrder.LITTLE_ENDIAN).getInt();
+                System.out.println("received msg number: " + msgNumber);
+
+                msgNumber += 1;
+
+                byte[] sendBuffer = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(msgNumber).array();
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, receivePacket.getAddress(), receivePacket.getPort());
+                socket.send(sendPacket);
+
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
